6af8eefb7cef608a543baebc53faf8b95e05993b
shriyansh98 <shriyansh.agrawal1998@gmail.com>
2023-08-17 18:36:26 +0530

    comments removes

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ pallate = [(202, 110, 164), (240, 241, 245), (236, 243, 239), (149, 50, 75), (22
  (138, 20, 31), (134, 184, 163), (197, 73, 92), (47, 86, 121), (73, 35, 43), (145, 149, 178), (14, 70, 98), (232, 165, 176), 
  (160, 158, 142), (54, 50, 45), (101, 77, 75), (183, 171, 205), (36, 74, 60), (19, 89, 86), (82, 129, 148), (147, 19, 17),
  (27, 102, 68), (12, 64, 70), (107, 153, 127), (176, 208, 192), (168, 102, 99)]
-# squirtal.color("red")
 
-# angle= 360
+
+
 turn_angle = 90
 space_size = 50
 dot_size = 20
@@ -21,24 +21,24 @@ no_of_dots =10
 def line_func(no_dots):
     squirtal.dot(dot_size,choice(pallate))
     if (no_of_dots-1)>no_dots:
-        # squirtal.penup()
+        
         squirtal.forward(space_size)
-        # squirtal.pendown()
+        
         
 
 
 def next_row_left():
     squirtal.left(turn_angle)
-    # squirtal.penup()
+    
     squirtal.forward(space_size)
-    # squirtal.dot(dot_size)
+    
     squirtal.left(turn_angle)
     
 def next_row_right():
     squirtal.right(turn_angle)
-    # squirtal.penup()
+    
     squirtal.forward(space_size)
-    # squirtal.dot(dot_size)
+    
     squirtal.right(turn_angle)
 
 squirtal.setheading(225)
@@ -48,13 +48,13 @@ squirtal.forward(50)
 
 while rows != 10:
     for i in range(0,no_of_dots):
-        # squirtal.forward(100)
-        # # squirtal.penup()
-        # # squirtal.forward(5)
-        # # squirtal.pendown()
-        # # squirtal.forward(5)
-        # squirtal.left(angle/sides)
-        # squirtal.color(choice(pallate))
+        
+        
+        
+        
+        
+        
+        
         line_func(i)
     if rows%2 == 0: 
         next_row_left()
@@ -62,7 +62,7 @@ while rows != 10:
         next_row_right()
 
     rows += 1 
-    # squirtal.left(turn_angle)
+    
 
     
     
